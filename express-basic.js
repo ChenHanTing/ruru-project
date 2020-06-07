e0c01cc17539942ad2d2ca814e8ce57b57b1a3a3
Han <hantingchen@eslite.com>
2020-06-07 19:32:41 +0800

    express diary new, show, index and add joi

--- express-basic.js
+++ express-basic.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+// const Joi = require("@hapi/joi");
+const Joi = require("joi");
+
+// use the middleware to access the popeline
+app.use(express.json());
+
+const diaries = [
+  { id: 1, content: "Hello01" },
+  { id: 2, content: "Hello02" },
+  { id: 3, content: "Hello03" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -9,10 +21,67 @@ app.get("/api/hello", (req, res) => {
   res.send(new Array(2).fill("Ruru love Han!"));
 });
 
-app.get("/api/example", (req, res) => {
+app.get("/api/examples", (req, res) => {
   res.send(new Array(2).fill("Han love Ruru!"));
 });
 
-app.listen(3100, () => {
-  console.log("Listening on port 3100...");
+app.get("/api/examples/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/diaries", (req, res) => {
+  res.send(diaries);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  // http://localhost:5000/api/posts/1994/05?sortBy=name
+  res.send(req.params); // params
+  res.send(req.query); // query strings
+});
+
+app.get("/api/diaries/:id", (req, res) => {
+  const diary = diaries.find((d) => d.id === parseInt(req.params.id));
+  if (!diary)
+    res.status(404).send("The diary with the given id was not found.");
+
+  res.send(diary);
+});
+
+app.post("/api/diaries", (req, res) => {
+  const schema = {
+    content: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  // console.log(result);
+
+  if (result.error) {
+    res.status("400").send(result.error.details[0].message);
+    return;
+  }
+  // Joi用法: https://www.npmjs.com/package/joi
+
+  /**
+   * if (!req.body.content || req.body.content.length < 3) {
+   *    // 400 Bad Request in convention
+   *    res
+   *      .status("400")
+   *      .send("Content is required and should be minimum 3 characters.");
+   *    return;
+   *  }
+   */
+
+  const diary = {
+    id: diaries.length + 1,
+    name: req.body.content,
+  };
+
+  diaries.push(diary);
+  res.send(diary);
+});
+
+// HARD-CODE => app.listen(3100, () => { ...
+const port = process.env.PORT || 3100;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
